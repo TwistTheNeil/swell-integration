7e9dd91ffab01988e40d3557091a59b636312416
Neil Castelino <neil.fl93@gmail.com>
2016-03-02 20:37:01 -0500

    added more debug statements for testing

--- TestDLL.cpp
+++ TestDLL.cpp
@@ -16,13 +16,22 @@ extern "C"
 	}
 
 	void* setDataNegative(char* a, int size, unsigned int& responseSize) {
-		swellanimations::Node* node = new swellanimations::Node();
-	//	Node *node = (Node*)calloc(1, sizeof(Node));
-		node->ParseFromArray(a, size);
+		printf("test1\n");
+//		swellanimations::Node* node = new swellanimations::Node();
+		printf("test2\n");
+		Node *node = (Node*)calloc(1, sizeof(Node));
+		printf("what\n");
+		printf("%s--\n", a);
+		node->ParseFromArray(a, strlen(a));
+		printf("test3\n");
 		iterate(node);
+		printf("test4\n");
 		responseSize = node->ByteSize();
+		printf("test5\n");
 		void* response = malloc(responseSize);
+		printf("test6\n");
 		node->SerializeToArray(response, responseSize);
+		printf("test7\n");
 		return response;
 	}
 
