c213112a6884d18a17516336018b3eb37f73f541
Neil Castelino <neil.fl93@gmail.com>
2016-03-02 20:03:49 -0500

    added testdll

--- TestDLL.cpp
+++ TestDLL.cpp
@@ -1,13 +1,13 @@
+#include <stdio.h>
 #include <iostream>
 #include <sstream>
+#include <string.h>
 #include "modeldata.pb.h"
+
+using namespace swellanimations;
 
 extern "C"
 {
-	void main() {
-		printf("hello");
-	}
-
 	void iterate(swellanimations::Node* node) {
 		//node->set_data(node->data() / -2);
 		//if (node->has_childnode()) {
@@ -17,6 +17,7 @@ extern "C"
 
 	void* setDataNegative(char* a, int size, unsigned int& responseSize) {
 		swellanimations::Node* node = new swellanimations::Node();
+	//	Node *node = (Node*)calloc(1, sizeof(Node));
 		node->ParseFromArray(a, size);
 		iterate(node);
 		responseSize = node->ByteSize();
@@ -24,4 +25,15 @@ extern "C"
 		node->SerializeToArray(response, responseSize);
 		return response;
 	}
+
+	int main() {
+		printf("hello");
+		char *s = (char*)calloc(9, sizeof(char));
+		strncpy(s, "CgJzMA==", 8);
+		unsigned int a = 8;
+		setDataNegative(s, 8, a);
+		return 0;
+	}
+
+
 }
