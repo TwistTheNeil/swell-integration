f369591f9fe24fbd965bd8c66175b267b3bbdac8
Angel Delgado <angeldelgado7@gmail.com>
2016-03-03 21:26:01 -0500

    fixed bug with char* size

--- swell-integration.cpp
+++ swell-integration.cpp
@@ -23,31 +23,12 @@ extern "C" {
 	__declspec (dllexport) 
 	#endif
 	void* setDataNegative(char* a, int size, unsigned int& responseSize) {
-		printf("test1\n");
 		Node* node = new Node();
-		printf("test2\n");
-		//		Node *node = (Node*)calloc(1, sizeof(Node));
-		printf("what\n");
-		printf("%s--\n", a);
-		node->ParseFromArray(a, strlen(a));
-		printf("test3\n");
+		node->ParseFromArray(a, size);
 		iterate(node);
-		printf("test4\n");
 		responseSize = node->ByteSize();
-		printf("test5\n");
 		void* response = malloc(responseSize);
-		printf("test6\n");
 		node->SerializeToArray(response, responseSize);
-		printf("test7\n");
 		return response;
-	}
-
-	int main() {
-		printf("hello");
-		char *s = (char*)calloc(9, sizeof(char));
-		strncpy(s, "CgJzMA==", 8);
-		unsigned int a = 8;
-		setDataNegative(s, 8, a);
-		return 0;
 	}
 }
