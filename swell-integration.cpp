c2a0ea71cbd9fb1bdcd5e9d7289644d823123a36
Sarah Kushner <sak338@drexel.edu>
2016-03-06 22:03:08 -0500

    adding animation generation files for the includes in swell-integration.cpp

--- swell-integration.cpp
+++ swell-integration.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string.h>
 #include "swell-integration.h"
+#include "animate/AnimationGeneration.cpp"
 #include "AnimationDataSerializer/modeldata.pb.h"
 
 using namespace swellanimations;
@@ -21,7 +22,8 @@ Animation* generateTestData(ModelData* modelData) {
 void* generateAnimation(char* a, int size, unsigned int& responseSize) {
     ModelData* modelData = new ModelData();
     modelData->ParseFromArray(a, size);
-    Animation* animation = generateTestData(modelData);
+    //Animation* animation = generateTestData(modelData);
+    Animation* animation = getFrames(modelData);
     responseSize = animation->ByteSize();
     void* response = malloc(responseSize);
     animation->SerializeToArray(response, responseSize);
